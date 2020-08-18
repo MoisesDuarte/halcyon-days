423b2b32126513af1d88d3c04f4f68ee990a8896
MoisesDuarte <moisesduartejunior@gmail.com>
2020-08-17 23:13:09 -0300

    Writing utility function for page turning (a bit messy, should clean it up later)

---
+++ js/page.util.js
@@ -0,0 +1,31 @@
+// TODO: Clean up this code. Learn some code golfing in the meantime.
+// Get the number of paragraphs in the page and the page counter
+mainContainer = document.querySelector(".content");
+pageCounter = document.querySelector("#page_number");
+paragraphNumber = mainContainer.getElementsByTagName("p").length;
+
+function pageTurn(direction) {
+    // Getting the current paragraph
+    currentParagraph = document.querySelector("p:not(.hide)");
+    nextId = parseInt(currentParagraph.id) + 1;
+    prevId = parseInt(currentParagraph.id) - 1;
+
+    if (direction == 'next') {
+        // Checking if there is a next one
+        if (nextId <= paragraphNumber) {
+            currentParagraph.className = "hide";
+            nextParagraph = document.getElementById(nextId);
+            nextParagraph.className = "";
+            pageCounter.innerHTML++;
+        }  
+    } else if (direction == 'back') {
+        // Checking if there is a previous one
+        if (prevId > 0) {
+            currentParagraph.className = "hide";
+            prevParagraph = document.getElementById(prevId);
+            prevParagraph.className = "";
+            pageCounter.innerHTML--;
+        } 
+    }
+     
+}
